5a573d6535d760a9e8ec9d4b33ac9a727e1e86ee
Shiva Eslami <shivaslami@gmail.com>
2022-04-05 00:44:52 +0430

    Add item details page

---
+++ js/pages/item-details.js
@@ -0,0 +1,47 @@
+//// countdown
+function countDownTimer() {
+    const difference = +new Date("2022-09-01") - +new Date();
+    let remaining = "Time's up!";
+
+    if (difference > 0) {
+        const parts = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60),
+        };
+        remaining = Object.keys(parts).map(part => {
+            return `${parts[part]}`;
+        }).join(" : ");
+    }
+
+    document.getElementById("countdown").innerHTML = remaining;
+}
+
+setInterval(countDownTimer, 1000);
+
+
+/////// tabs
+const tabs = document.querySelectorAll('.tab__item');
+const tabContents = document.querySelectorAll('.tab__content');
+
+// Functions
+const activateTab = tabnum => {
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    })
+
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active');
+    })
+
+    document.querySelector('#tab' + tabnum).classList.add('active');
+    document.querySelector('#tab_content' + tabnum).classList.add('active');
+}
+// Event Listeners
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        activateTab(tab.dataset.tab)
+    })
+})
--- js/script.js
+++ js/script.js
@@ -40,3 +40,35 @@ let overlay = document.getElementById("preloader");
 window.addEventListener('load', function () {
     overlay.style.display = 'none';
 })
+
+///// mobile menu
+const menu = document.querySelector('.navbar');
+const menuTrigger = document.querySelector('.menu-btn');
+const menuClosed = menu.querySelector('.menu-mobile-close');
+const menuItem = menu.querySelector('.nav__list');
+let subMenu;
+
+menuItem.addEventListener('click', (e) => {
+    if (e.target.closest('.has-children')) {
+        const hasChildren = e.target.closest('.has-children');
+        showSubMenu(hasChildren);
+    }
+});
+
+menuTrigger.addEventListener('click', () => {
+    toggleMenu();
+});
+menuClosed.addEventListener('click', () => {
+    toggleMenu();
+});
+function toggleMenu() {
+    menu.classList.toggle('active');
+}
+function showSubMenu(hasChildren) {
+    subMenu = hasChildren.querySelector('.nav__child');
+    subMenu.classList.add('active');
+    subMenu.style.animation = 'slide-toLeft 0.5s ease forwards';
+    const menuTitle = hasChildren.querySelector('i').parentNode.childNodes[0].textContent;
+    menu.querySelector('.menu-mobile-title').innerHTML = menuTitle;
+    menu.querySelector('.menu-mobile-header').classList.add('active');
+}
